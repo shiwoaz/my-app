b65121a0689a5eef719105a76c9668105ee83ab5
ismeovo <1219353840@qq.com>
2021-05-29 10:49:55 +0800

    feat(tailwinds)add tailwinds config

--- draco/tailwind.config.js
+++ draco/tailwind.config.js
@@ -1,11 +1,18 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: (theme) => ({
+        chat: "url(https://ftp.bmp.ovh/imgs/2021/05/4e1e0aa8d00d0cc7.jpeg)",
+      }),
+    },
+    minWidth: {
+      "1/4": "25%",
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
