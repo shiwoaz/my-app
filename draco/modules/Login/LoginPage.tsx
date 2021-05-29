4c531629c2de2c7974e777a989a30264188dbc9e
ismeovo <1219353840@qq.com>
2021-05-30 00:38:07 +0800

    :sparkles:feat(login)response css

--- draco/modules/Login/LoginPage.tsx
+++ draco/modules/Login/LoginPage.tsx
@@ -20,20 +20,20 @@ const Login: React.FC = () => {
         }}
       >
         <div
-          className='w-3/5 h-3/5 flex items-center bg-gray-800 bg-opacity-10 shadow-xl rounded-lg'
+          className='md:w-3/5 w-5/6 md:h-3/5 h-5/6 flex md:flex-row flex-col items-center bg-gray-800 bg-opacity-10 shadow-xl rounded-lg'
           style={{
             'backdropFilter': 'blur(5px)'
           }}
         >
           <div
-            className='bg-chat w-2/5 h-4/6 rounded-md m-14'
+            className='bg-chat md:w-2/5 md:h-4/6 h-2/4 rounded-md md:m-14 mt-7 w-4/5'
           ></div>
-          <div className='h-full flex w-5/6 flex-col justify-center content-between'>
-            <div className='h-2/6'>
-              <h1 className='text-3xl font-bold text-center'>Get Started Now</h1>
+          <div className='h-full flex w-5/6 flex-col md:justify-center content-between justify-around'>
+            <div className='h-2/6 flex flex-col justify-center '>
+              <h1 className='ext-3xl font-bold text-center text-lg text-gray-900'>Get Started Now</h1>
               <p className='text-center mt-5 font-mono'>It's free to join!</p>
             </div>
-            <div className='h-3/6 flex flex-col justify-around items-center w-full'>
+            <div className='md:h-3/6 h-1/3 flex flex-col justify-around items-center w-full'>
               <LoginButton href={oauthURL('GITHUB')}>
                 <GithubSvg />
                 <span>Log in with Github</span>
--- draco/ui/Button.tsx
+++ draco/ui/Button.tsx
@@ -2,14 +2,18 @@ import React from 'react'
 
 export type ButtonProps = {
   onClick?: React.MouseEventHandler<HTMLButtonElement>
+  style?: React.CSSProperties
+  className?: string
 }
 
 const Button: React.FC<ButtonProps> = ({
   children,
-  onClick
+  onClick,
+  style,
+  className
 }) => {
 
-  return <button onClick={onClick}>{children}</button>
+  return <button className={className} style={style} onClick={onClick}>{children}</button>
 }
 
 export default Button
@@ -26,7 +30,7 @@ export const LoginButton: React.FC<LoginButtonProps> = ({ children, href }) => {
   }
 
   return (
-    <Button onClick={onClick}>
+    <Button onClick={onClick} className='p-3 md:p-0 md:h-1/5'>
       <div className='flex flex-nowrap w-52 min-w-1/4 justify-around p-4 border-2 rounded-md border-blue-300 border-opacity-75 hover:bg-blue-500 hover:bg-opacity-10'>
         {children[0]}
         {children[1]}
