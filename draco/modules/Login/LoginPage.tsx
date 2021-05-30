bb330c589f976030920d3991c998baeac8f223f5
ismeovo <1219353840@qq.com>
2021-05-30 18:41:53 +0800

    :sparkles:feat(login)other style

--- draco/modules/Login/LoginPage.tsx
+++ draco/modules/Login/LoginPage.tsx
@@ -16,13 +16,14 @@ const Login: React.FC = () => {
       <div
         className='w-screen h-screen flex justify-center items-center min-w-1/4'
         style={{
-          "backgroundImage": "linear-gradient(to right, #2980b9, #6dd5fa, #ffffff)"
+          "backgroundImage": "linear-gradient(to right, rgb(35, 48, 66), rgb(35, 48, 66))"
         }}
       >
         <div
-          className='md:w-3/5 w-5/6 md:h-3/5 h-5/6 flex md:flex-row flex-col items-center bg-gray-800 bg-opacity-10 shadow-xl rounded-lg'
+          className='md:w-3/5 w-5/6 md:h-3/5 h-5/6 flex md:flex-row flex-col items-center bg-opacity-25 shadow-xl rounded-lg'
           style={{
-            'backdropFilter': 'blur(5px)'
+            'backdropFilter': 'blur(5px)',
+            "backgroundImage": "linear-gradient(to right, #2d3e55, #2d3e59)"
           }}
         >
           <div
@@ -30,16 +31,16 @@ const Login: React.FC = () => {
           ></div>
           <div className='h-full flex w-5/6 flex-col md:justify-center content-between justify-around'>
             <div className='h-2/6 flex flex-col justify-center '>
-              <h1 className='ext-3xl font-bold text-center text-lg text-gray-900'>Get Started Now</h1>
-              <p className='text-center mt-5 font-mono'>It's free to join!</p>
+              <h1 className='ext-3xl font-bold text-center text-lg text-pink-100'>Get Started Now</h1>
+              <p className='text-center mt-5 font-mono text-indigo-50'>It's free to join!</p>
             </div>
             <div className='md:h-3/6 h-1/3 flex flex-col justify-around items-center w-full'>
               <LoginButton href={oauthURL('GITHUB')}>
-                <GithubSvg />
+                <GithubSvg classname={'text-indigo-50 fill-current'} />
                 <span>Log in with Github</span>
               </LoginButton>
               <LoginButton href={`${BaseURL}`}>
-                <TwitterSvg />
+                <TwitterSvg classname={'text-indigo-50 fill-current'} />
                 <span>Log in with Twitter</span>
               </LoginButton>
             </div>
