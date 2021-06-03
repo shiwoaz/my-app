3ad6a1f713409cde4ad62e772fe1a98cfeefa2cc
PengXiaohao <1219353840@qq.com>
2021-06-03 16:18:18 +0800

    :sparkles:feat(main-contaniner) rooms get api

--- draco/ui/MainContainer.tsx
+++ draco/ui/MainContainer.tsx
@@ -1,4 +1,8 @@
-import * as React from 'react';
+import { GetServerSideProps } from 'next';
+import React, { useContext, useEffect, useState } from 'react';
+
+import SocketContext from '../modules/content/socket'
+import { APIURL } from '../settings/Global';
 
 interface IMainContainer {
 }
@@ -7,11 +11,35 @@ const MainContainer: React.FC<IMainContainer> = ({
 
 }) => {
 
+  const { io } = useContext(SocketContext)
+
+  console.log(io, 123);
+
+  const [rooms, setRooms] = useState<Array<any>>([])
+
+  const getRooms = async () => {
+    const res = await fetch(APIURL + '/room/query')
+    const c = await res.json()
+    setRooms(c)
+  }
+
+  useEffect(() => {
+    getRooms()
+    return () => {
+
+    }
+  }, [])
+
 
   return (
     <>
       <div id="container_m" className='h-4/6 shadow-xl w-11/12 mx-auto bg-roomBgC bg-opacity-50 p-2 rounded-md overflow-scroll'>
         123
+        {
+          rooms.map(item => (
+            <span>{item[0]}</span>
+          ))
+        }
       </div>
     </>
   )
