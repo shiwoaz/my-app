5ab9a9f640231f947413ce7a5c85a48dcb76ad39
PengXiaohao <1219353840@qq.com>
2021-06-03 16:17:12 +0800

    :bug:fix(socket provider)fix

--- draco/modules/Socket.tsx
+++ draco/modules/Socket.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { io } from 'socket.io-client'
 
 import SocketProvider from './content/socket'
@@ -10,11 +10,15 @@ const Socket: React.FC = ({
 
   const { io: sock } = useContext(SocketProvider)
   //@ts-ignore
-  let ser: Socket<DefaultEventsMap, DefaultEventsMap> | null
+  // let ser: Socket<DefaultEventsMap, DefaultEventsMap> | null
+
+  const [ser, setSer] = useState(null)
 
   useEffect(() => {
 
-    ser = io(APIURL)
+    const ser = io(APIURL)
+
+    setSer(ser as unknown as any)
 
     ser.emit('join', {}, (err: any) => {
       console.log(err);
