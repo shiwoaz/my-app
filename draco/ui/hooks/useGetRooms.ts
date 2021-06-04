ce43fa6c5dd1a9b8575cf1998121a569afafa20c
PengXiaohao <1219353840@qq.com>
2021-06-04 17:27:33 +0800

    :sparkles:feat(socket)add select room show

--- draco/ui/hooks/useGetRooms.ts
+++ draco/ui/hooks/useGetRooms.ts
@@ -1,12 +1,12 @@
 import { useEffect, useState } from 'react'
 import { APIURL } from '../../settings/Global'
 
-const useGetRooms = (flag: boolean) => {
+const useGetRooms = (flag: boolean, roomId?: string) => {
 
   const [rooms, setRooms] = useState({})
 
   const getRooms = async () => {
-    const res = await fetch(APIURL + '/room/query')
+    const res = await fetch(APIURL + '/room/query' + (roomId ? `?roomId=${roomId}` : ''))
     const c = await res.json()
     setRooms(c)
   }
