5e6bc820a24b3ead53fe6feb29399fc104933682
PengXiaohao <1219353840@qq.com>
2021-06-04 14:50:26 +0800

    :bug:fix(roomcard)avatar display flex

--- draco/ui/RoomCard.tsx
+++ draco/ui/RoomCard.tsx
@@ -6,10 +6,12 @@ import Avatar from './Avatar';
 
 interface IRoomCard {
   room: info[]
+  onClick: () => void
 }
 
 const RoomCard: React.FC<IRoomCard> = ({
-  room
+  room,
+  onClick
 }) => {
 
   console.log(room, "room");
@@ -18,12 +20,12 @@ const RoomCard: React.FC<IRoomCard> = ({
   return (
     <>
       <Card className='bg-roomCard bg-opacity-50 mt-3 mb-3'>
-        <div className='flex flex-col '>
+        <div className='flex flex-col' onClick={onClick}>
           <span className='text-pink-100 text-lg font-mono subpixel-antialiased font-extrabold'>{room[0]['rooms']}</span>
           <div className='w-full h-16 flex flex-nowrap gap-2 items-center justify-between'>
-            <div className='ml-3'>
+            <div className='ml-3 flex '>
               {
-                room.slice(0, room.length > 5 ? 5 : room.length).map(({ user_avatar }) => <Avatar className='h-8 w-8 -ml-3' url={user_avatar} />)
+                room.slice(0, room.length > 5 ? 5 : room.length).map(({ id, user_avatar }) => <Avatar key={id} className='h-8 w-8 -ml-3 block' url={user_avatar} />)
               }
             </div>
             <div className='flex gap-2 justify-center items-center'>
