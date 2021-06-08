50f2921ba805c42e4e1d88d2ce66911480f9a6bb
PengXiaohao <1219353840@qq.com>
2021-06-08 16:05:52 +0800

    :bug:fix(ui/people)green point position

--- draco/ui/PeopleCard.tsx
+++ draco/ui/PeopleCard.tsx
@@ -19,13 +19,15 @@ const PeopleCard: React.FC<IPeopleCard> = ({
 
   return (
     <>
-      <Card className=' bg-gray-500 flex flex-nowrap items-center h-auto mt-2 mb-2 gap-1 text-sm'>
-        <div>
-          <Avatar url={avatar} height='2.5rem' width='2.5rem' />
-        </div>
-        <div className='flex flex-col justify-between'>
-          <span>{name}</span>
-          <span className='truncate'><span className='mr-1'>{t('main.position')}:</span>{room ? <span>{room}</span> : <span>{t('main.hall')}</span>}</span>
+      <Card className=' bg-gray-500 flex flex-nowrap items-center h-auto mt-2 mb-2 gap-1 text-sm justify-between'>
+        <div className='flex flex-nowrap w-auto gap-2'>
+          <div>
+            <Avatar url={avatar} height='2.5rem' width='2.5rem' />
+          </div>
+          <div className='flex flex-col justify-between'>
+            <span>{name}</span>
+            <span className='truncate'><span className='mr-1'>{t('main.position')}:</span>{room ? <span>{room}</span> : <span>{t('main.hall')}</span>}</span>
+          </div>
         </div>
         <div className='w-2 h-2 bg-green-500 bg-opacity-75 rounded-full ml-2.5'></div>
       </Card>
