02571dbcb3e489175b62494519e196f1ec94c69d
PengXiaohao <1219353840@qq.com>
2021-06-08 16:38:01 +0800

    :sparkles:feat(chat)io emit add

--- draco/ui/ChatMain.tsx
+++ draco/ui/ChatMain.tsx
@@ -1,15 +1,24 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
 import { useRouter } from 'next/router'
 
 import useGetRooms from './hooks/useGetRooms'
+import SocketProvider from '../modules/content/socket'
 
 interface IChatMain {
 
 }
 
-const ChatMain: React.FC = ({ }) => {
+const ChatMain: React.FC<IChatMain> = ({ }) => {
 
+  const { io } = useContext(SocketProvider)
 
+  useEffect(() => {
+
+    return () => {
+      io?.emit('leaveRoom')
+      console.log("leaveroom");
+    }
+  }, [])
 
 
   return (
